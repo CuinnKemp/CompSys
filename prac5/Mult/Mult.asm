cc166964c104ce6e0f058209162fa971542e763a
CuinnKemp <CuinnKemp@outlook.com>
2023-09-14 20:45:21 +0930

    Multiplier made

--- prac5/Mult/Mult.asm
+++ prac5/Mult/Mult.asm
@@ -7,3 +7,50 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+D=M
+@Positive
+D;JGT
+
+D = !D
+D = D + 1
+
+(Positive)
+@counter
+M = D
+
+@R0
+M = 0
+
+
+(Loop)
+@R1
+D = M
+@R0
+M = M + D
+
+@counter
+M = M - 1
+D = M
+
+@Loop
+D;JGT
+
+@R2
+D = M
+
+@NEG
+D;JLT
+
+(End)
+@End
+A;JMP
+
+(NEG)
+@R0
+M=!M
+M=M+1
+@End
+A;JMP
+
+
