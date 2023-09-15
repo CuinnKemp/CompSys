9cc2050236c7db44ff676be50d64ebbad4edd138
CuinnKemp <CuinnKemp@outlook.com>
2023-09-15 12:41:59 +0930

    idek aye

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,74 +1,66 @@
 // Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+
 // Put your code here.
+//Check if array start far above R2
+@R1
+D=M
 
-    // Checking if the start of the array is past the 2nd postiion
-    @R1
-    D=M
-    D=D-1
-    D=D-1
-    @END
-    D;JLT
-    //Resetting it back to how it was
-    D=D+1
-    D=D+1
-    
-    // Sets R0 to R1 (base case)
-    @R0
-    M=D
-    
-    @R2
-    D=M
-    //Sends to the end if Array size is less than 1
-    @END
-    D;JLT
+@END
+D=D-1
+D=D-1
+D;JLE
 
+//get current tempMin
+@R1
+A=M
+D=M
 
-    // Three Sections, Main Loop, If smaller, Next element
-(MAIN_LOOP)
+@R0
+M=D
 
-    // Sets next address
-    @R1
-    A=M
-    D=M
-    
+//Check that length of array is sufficient
+@R2
+D=M
 
-    // Current element vs smallest found so far
-    @R0 
-    D=D-M
-
-    @IF_SMALLER
-    D;JLT
-
-    @NEXT_ELEMENT
-    0;JMP
+@END
+D;JLE
 
 
-(IF_SMALLER)
-    // Sets R0 to R1
-    @R1
-    A=M
-    D=M
-    @R0
-    M=D
 
-(NEXT_ELEMENT)
+(Loop)
+@R2
+D = M
 
-    // Iterates R1
-    @R1
-    M=M+1
+@R1
+D=D-1
+A = M + D
+D = M
 
-    //Decrements R2
-    @R2
-    M=M-1
+@R0
+D = M-D
 
-    // Goes back and does it again if R2 > 0
-    @R2
-    D=M
-    @MAIN_LOOP
-    D;JGT 
+@keepTemp
+D;JLT
+@R2
+D=M
+
+@R1
+D=D-1
+A = M + D
+D = M
+
+@R0
+M=D
+
+(keepTemp)
+@R2
+M = M-1
+D = M
+
+@Loop
+D;JGT
 
 (END)
-    @END
-    0;JMP    
-
+@END
+A;JMP
