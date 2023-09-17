540440ccaabac046b7611955bd01b88864769ff6
QGKemp <CuinnKemp@outlook.com>
2023-09-17 14:08:04 +0930

    back to old

--- prac5/ArrSort/ArrSort.asm
+++ prac5/ArrSort/ArrSort.asm
@@ -1,4 +1,4 @@
-// Check if array start far above R2
+//Check if array start far above R2
 @R1
 D=M
 @R2
@@ -12,180 +12,187 @@ D;JLT
 D=M
 D=D-1
 D=D-1
+
 @Sorted
 D;JLE
 
-// Check that the length of the array is sufficient
+//Check that length of array is sufficient
 @R2
 D=M
+
 @Sorted
 D;JLE
 
-// Main Loop
+
+
+
 (MainLoop)
 @R1
-D=M
+D = M
 @R3
-M=D
-
-// Switch Loop
+M = D
 (switchLoop)
 @R3
-A=M
-D=M
+A = M
+D = M
+
 @swAPOS
-D;JLE
+D;JGE
 
 @swANEG
 D;JMP
 
-// Handle swAPOS case
 (swAPOS)
 @R3
-A=M+1
-D=M
+A = M + 1
+D = M
+
 @swChangeOrder
-D;JGT
+D;JLT
 
 @swNNPP
 D;JMP
 
-// Handle swANEG case
 (swANEG)
 @R3
-A=M+1
-D=M
+A = M + 1
+D = M
+
 @swNNPP
-D;JGT
+D;JLT
 
 @swkeepOrder
 D;JMP
 
-// Handle swNNPP case
 (swNNPP)
 @R3
-A=M
-D=M
-A=A+1
-D=M-D
+A = M
+D = M
+A = A + 1
+D = M - D
+
 @swkeepOrder
 D;JGE
 
-// Handle swChangeOrder case
 (swChangeOrder)
 @R3
-A=M
-D=M
+A = M
+D = M
+
 @temp
-M=D
+M = D
 
 @R3
-A=M
-A=A+1
-D=M
+A = M
+A = A + 1
+D = M
 @temptwo
-M=D
+M = D
 
 @R3
-A=M
-M=D
+A = M
+M = D
 
 @temp
-D=M
+D = M
 
 @R3
-A=M
-A=A+1
-M=D
+A = M
+A = A + 1
+M = D
 
 (swkeepOrder)
 @R3
-M=M+1
+M = M + 1
 
 @R1
-D=M
+D = M
+
 @R2
-D=D-1
-D=D+M
+D = D - 1
+D = D + M
 
 @R3
-D=D-M
+D = D - M
 
 @switchLoop
 D;JNE
 
 // CHECK IF SORTED
 @R1
-D=M
+D = M
 
 @R3
-M=D
-
+M = D
 (scLoop)
 @R3
-A=M
-D=M
+A = M
+D = M
 
-// Handle APOS case
 @APOS
-D;JLE
+D;JGE
 
 @ANEG
 D;JMP
 
-// Handle APOS case
 (APOS)
 @R3
-A=M+1
-D=M
+A = M + 1
+D = M
+
 @MainLoop
 D;JLT
 
 @NNPP
 D;JMP
 
-// Handle ANEG case
 (ANEG)
 @R3
-A=M+1
-D=M
+A = M + 1
+D = M
+
 @NNPP
 D;JLT
 
 @Continue
 D;JMP
 
-// Handle NNPP case
 (NNPP)
 @R3
-A=M
-D=M
-A=A+1
-D=M-D
+A = M
+D = M
+A = A + 1
+D = M - D
 
 @MainLoop
 D;JLT
 
 (Continue)
 @R3
-M=M+1
+M = M + 1
 
 @R1
-D=M
+D = M
+
 @R2
-D=D-1
-D=D+M
+D = D - 1
+D = D + M
 
 @R3
-D=D-M
+D = D - M
 
 @scLoop
 D;JNE
 
 (Sorted)
 @R0
-M=-1
+M = -1
 
 (END)
 @END
-0;JMP
+A;JMP
+
+
+
+
+
